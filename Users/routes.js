ae4959c966cb0820ff99ce4415db55fb759f589e
arps <arpanp99@gmail.com>
2024-04-17 19:48:47 -0400

    4 completed

--- Users/routes.js
+++ Users/routes.js
@@ -1,17 +1,16 @@
 import * as dao from "./dao.js";
-let currentUser = null;
 export default function UserRoutes(app) {
   const createUser = async (req, res) => {
+    console.log(req.body);
+    delete req.body._id;
     const user = await dao.createUser(req.body);
     res.json(user);
   };
-
   const deleteUser = async (req, res) => {
     console.log(req.params);
     const status = await dao.deleteUser(req.params.userId);
     res.json(status);
   };
-
   const findAllUsers = async (req, res) => {
     const { role } = req.query;
     if (role) {
@@ -23,7 +22,6 @@ export default function UserRoutes(app) {
     res.json(users);
     return;
   };
-
   const findUserById = async (req, res) => {
     const user = await dao.findUserById(req.params.userId);
     res.json(user);
@@ -31,7 +29,7 @@ export default function UserRoutes(app) {
   const updateUser = async (req, res) => {
     const { userId } = req.params;
     const status = await dao.updateUser(userId, req.body);
-    currentUser = await dao.findUserById(userId);
+    req.session["currentUser"] = await dao.findUserById(userId);
     res.json(status);
   };
 
@@ -40,20 +38,36 @@ export default function UserRoutes(app) {
     if (user) {
       res.status(400).json({ message: "Username already taken" });
     }
-    currentUser = await dao.createUser(req.body);
+    const currentUser = await dao.createUser(req.body);
+    req.session["currentUser"] = currentUser;
+
     res.json(currentUser);
   };
   const signin = async (req, res) => {
     const { username, password } = req.body;
-    currentUser = await dao.findUserByCredentials(username, password);
-    res.json(currentUser);
+    const currentUser = await dao.findUserByCredentials(username, password);
+    console.log(req);
+    console.log(currentUser);
+    if (currentUser) {
+      console.log("here!!!");
+      req.session["currentUser"] = currentUser;
+      res.json(currentUser);
+    } else {
+      res.sendStatus(401);
+    }
   };
   const signout = (req, res) => {
-    currentUser = null;
+    req.session.destroy();
     res.sendStatus(200);
   };
 
   const profile = async (req, res) => {
+    const currentUser = req.session["currentUser"];
+    if (!currentUser) {
+      res.sendStatus(401);
+      return;
+    }
+
     res.json(currentUser);
   };
   app.post("/api/users", createUser);
